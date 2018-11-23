2345fdbd1903e63ae8c1a39e1ef746d0c43f0b48
grahmnic <36274445+grahmnic@users.noreply.github.com>
2018-11-23 16:03:10 -0500

    Added stuff
    Related Work Items: #1

--- LolEsports/ClientApp/app/app.browser.module.ts
+++ LolEsports/ClientApp/app/app.browser.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppModuleShared } from './app.shared.module';
 import { AppComponent } from './components/app/app.component';
-
 @NgModule({
     bootstrap: [ AppComponent ],
     imports: [
---
+++ LolEsports/ClientApp/app/components/sign-in/signin.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'signin',
+    templateUrl: './signin.component.html',
+    styleUrls: ['./signin.component.css']
+})
+export class SignInComponent implements OnInit{
+    constructor() {
+
+    }
+
+    ngOnInit() {
+
+    }
+}
