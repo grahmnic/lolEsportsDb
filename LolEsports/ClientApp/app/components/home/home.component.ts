936af049ae4ba2f1d3e548e25bda0139a695453f
grahmnic <36274445+grahmnic@users.noreply.github.com>
2018-11-30 17:47:37 -0500

    fixing home page, editing css of match and tournament page

--- LolEsports/ClientApp/app/components/home/home.component.ts
+++ LolEsports/ClientApp/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { DataService } from '../shared/dataservice';
 
 @Component({
@@ -6,10 +6,16 @@ import { DataService } from '../shared/dataservice';
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
     boxes1: any = [];
     boxes2: any = [];
     boxes3: any = [];
+    random_heights1: any = [];
+    random_heights2: any = [];
+    random_heights3: any = [];
+    length1: any = 0;
+    length2: any = 0;
+    length3: any = 0;
 
     constructor(private dataService: DataService) {
 
@@ -19,24 +25,45 @@ export class HomeComponent implements OnInit {
         window.open(link, "_blank");
     }
 
+    ngAfterViewInit() {
+
+    }
+
     randomHeight() {
-        return (Math.random() * 15) + 20;
+        var arr: number[] = new Array(this.length1);
+        for (var i = 0; i < this.length1; i++) {
+            arr[i] = (Math.random() * 10) + 20;
+        }
+        this.random_heights1 = arr;
+        var arr1: number[] = new Array(this.length2);
+        for (var i = 0; i < this.length2; i++) {
+            arr1[i] = (Math.random() * 10) + 20;
+        }
+        this.random_heights2 = arr1;
+        var arr2: number[] = new Array(this.length3);
+        for (var i = 0; i < this.length3; i++) {
+            arr2[i] = (Math.random() * 10) + 20;
+        }
+        this.random_heights3 = arr2;
     }
 
     ngOnInit() {
         this.dataService.getMedia()
             .subscribe((data) => {
                 var d = data.json();
-                console.log(d.length);
                 for (var i = 0; i < d.length; i++) {
                     if (i % 3 == 0) {
                         this.boxes1.push(d[i]);
+                        this.length1++;
                     } else if (i % 2 == 0) {
                         this.boxes2.push(d[i]);
+                        this.length2++;
                     } else {
                         this.boxes3.push(d[i]);
+                        this.length3++;
                     }
                 }
+                this.randomHeight();
             })
     }
 }
